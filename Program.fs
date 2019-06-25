adcb5fa837fe8c3fa65337a3a0c7d8d51d27b471
iranika <7370486+iranika@users.noreply.github.com>
2019-06-25 11:56:04 +0900

    add: テスト項目の追加

--- Program.fs
+++ Program.fs
@@ -9,7 +9,7 @@ canopy.configuration.chromeDir <- System.AppContext.BaseDirectory
 start chrome
 
 //this is how you define a test
-"モーダルチェックテスト" &&& fun _ ->
+"モーダル閉じる" &&& fun _ ->
     //this is an F# function body, it's whitespace enforced
 
     url "https://iranika.github.io/mo-code/index.html" //mo-codeのサイトを開く
@@ -17,20 +17,32 @@ start chrome
     click ".modal-label" //モーダルラベル(右上バツ)をクリック
     notDisplayed "#modal"   //モーダルが非表示になっている
 
+"最初から読む" &&& fun _ ->
     url "https://iranika.github.io/mo-code/index.html" //mo-codeのサイトを開く
     displayed "#modal"  //モーダルが表示されている
     click "最初から読む" //最初から読むをクリック
     notDisplayed "#modal"   //モーダルが非表示になっている
+    //対象ページの画像であることをチェックしたいけどいいアイデアが思い浮かばない//
     
+"新作から読む" &&& fun _ ->
     url "https://iranika.github.io/mo-code/index.html" //mo-codeのサイトを開く
     displayed "#modal"  //モーダルが表示されている
     click "新作から読む" //新作から読むをクリック
     notDisplayed "#modal"   //モーダルが非表示になっている
-
+    //対象ページの画像であることをチェックしたいけどいいアイデアが思い浮かばない//
+    
+"ハッシュでページ遷移" &&& fun _ ->
     url "https://iranika.github.io/mo-code/index.html#5" //mo-codeのサイトを開く#5のページ
     notDisplayed "#modal"   //モーダルが非表示になっている(ページ指定の場合はモーダル非表示が仕様)
+    //対象ページの画像であることをチェックしたいけどいいアイデアが思い浮かばない//
+    url "https://iranika.github.io/mo-code/index.html#latest" //mo-codeのサイトを開く#latestは最後のページを表示する仕様
+    notDisplayed "#modal"   //モーダルが非表示になっている(ページ指定の場合はモーダル非表示が仕様)
+    //対象ページの画像であることをチェックしたいけどいいアイデアが思い浮かばない//
 
-
+"スクロールによるオートロード" &&& fun _ ->
+    url "https://iranika.github.io/mo-code/index.html" //mo-codeのサイトを開く#5のページ
+    //いいアイデアがない//
+    
 //run all tests
 run()
 
