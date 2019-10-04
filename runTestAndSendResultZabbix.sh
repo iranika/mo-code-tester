323a0c84e07eb75d77444dfaa22e74d9bb630a50
iranika <7370486+iranika@users.noreply.github.com>
2019-10-04 09:01:20 +0900

    add: 環境変数を外部に切り出し

--- runTestAndSendResultZabbix.sh
+++ runTestAndSendResultZabbix.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -x
 
+#load env
+. runTest.conf.sh
+
 function SEND_ZABBIX(){
-    ZABBIX_SERVER="*********"
-    MONIT_HOSTNAME="WebTest"
-    MONIT_KEY="webtest.result"
     zabbix_sender -z $ZABBIX_SERVER -s $MONIT_HOSTNAME -k $MONIT_KEY -o "$1"
 }
 
 DISPLAY=:1 xvfb-run dotnet run --no-build | grep -v 'Connection refused Connection refused' > test.log
-grep -e "0 failed" test.log && SEND_ZABBIX "webtest was passed" || SEND_ZABBIX "webtest was failed"
+if grep -e "0 failed" test.log; then
+    SEND_ZABBIX 0
+else
+    SEND_ZABBIX 1
+    cp -pr test.log ./log/error-`date +%Y-%m-%dT%H%M%S`.log
+fi
 
